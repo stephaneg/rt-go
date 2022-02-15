a99b3f3ccd158faf158335f6945df35e5880728c
Stephane Gesuati <stephstephg@gmail.com>
2022-02-15 23:33:56 +0100

    complete canvas implem

--- core/canvas.go
+++ core/canvas.go
@@ -1,5 +1,10 @@
 package core
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Canvas struct {
 	Width  uint
 	Height uint
@@ -10,16 +15,56 @@ func NewCanvas(width, height uint) *Canvas {
 	return &Canvas{width, height, make([]Color, width*height)}
 }
 
-func (c *Canvas) write(width, height uint, color Color) {
+func (c *Canvas) Write(width, height uint, color Color) {
 	index := c.getIndex(width, height)
 	c.Pixels[index] = color
 }
 
-func (c *Canvas) read(width, height uint) Color {
+func (c *Canvas) Read(width, height uint) Color {
 	index := c.getIndex(width, height)
 	return c.Pixels[index]
 }
 
 func (c *Canvas) getIndex(x, y uint) uint {
 	return y*c.Width + x
+}
+
+func (c *Canvas) ToPPM() string {
+	var b strings.Builder
+	b.Grow(32)
+
+	fmt.Fprintf(&b, "P3\n")
+	fmt.Fprintf(&b, "%d %d\n", c.Width, c.Height)
+	fmt.Fprintf(&b, "%d\n", 255)
+
+	var current_col int = 0
+	var row_size int = 0
+
+	for _, p := range c.Pixels {
+		colors := p.ToRgb()
+
+		for _, color := range colors {
+			str := fmt.Sprintf(" %d", color)
+			fmt.Fprintf(&b, str)
+			row_size += len(str)
+
+			if row_size > 70 {
+				fmt.Fprintf(&b, "\n")
+				row_size = 0
+			}
+		}
+
+		// saut de ligne pour chaque ligne de la matrice de pixems
+		if current_col < int(c.Width)-1 {
+			current_col += 1
+		} else {
+			current_col = 0
+			row_size = 0
+			fmt.Fprintf(&b, "\n")
+
+		}
+
+	}
+
+	return b.String()
 }
--- core/color.go
+++ core/color.go
@@ -1,5 +1,9 @@
 package core
 
+import (
+	"math"
+)
+
 type Color struct {
 	R float64
 	G float64
@@ -28,4 +32,18 @@ func (c Color) MulColorF64(other float64) Color {
 
 func (c Color) FuzzyEqColor(other Color) bool {
 	return FuzzyEqf64(c.R, other.R) && FuzzyEqf64(c.G, other.G) && FuzzyEqf64(c.B, other.B)
+}
+
+func (c Color) ToRgb() []byte {
+	var rgb []byte
+	var r, g, b byte
+
+	r = (byte)(math.Round(math.Max(math.Min(c.R, 1.0), 0.0) * 255.0))
+	g = (byte)(math.Round(math.Max(math.Min(c.G, 1.0), 0.0) * 255.0))
+	b = (byte)(math.Round(math.Max(math.Min(c.B, 1.0), 0.0) * 255.0))
+	rgb = append(rgb, r)
+	rgb = append(rgb, g)
+	rgb = append(rgb, b)
+
+	return rgb
 }
