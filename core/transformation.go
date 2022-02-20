e7d658c7c438a99a46897ad3021e1ddf15d499bd
Stephane Gesuati <stephstephg@gmail.com>
2022-02-20 14:51:22 +0100

    transformation

--- core/transformation.go
+++ core/transformation.go
@@ -1,5 +1,7 @@
 package core
 
+import "math"
+
 // Translation matrix return a 4x4 translation matrix - the translation vector is (x, y, z)
 func TranslationMatrix(x, y, z float64) Matrix4 {
 	return NewMatrix4From([4][4]float64{
@@ -16,6 +18,33 @@ func ScaleMatrix(x, y, z float64) Matrix4 {
 		{x, 0.0, 0.0, 0.0},
 		{0.0, y, 0.0, 0.0},
 		{0.0, 0.0, z, 0.0},
+		{0.0, 0.0, 0.0, 1.0},
+	})
+}
+
+func RotationX(radian float64) Matrix4 {
+	return NewMatrix4From([4][4]float64{
+		{1.0, 0.0, 0.0, 0.0},
+		{0.0, math.Cos(radian), -math.Sin(radian), 0.0},
+		{0.0, math.Sin(radian), math.Cos(radian), 0.0},
+		{0.0, 0.0, 0.0, 1.0},
+	})
+}
+
+func RotationY(radian float64) Matrix4 {
+	return NewMatrix4From([4][4]float64{
+		{math.Cos(radian), 0.0, math.Sin(radian), 0.0},
+		{0.0, 1.1, 0.0, 0.0},
+		{-math.Sin(radian), 0.0, math.Cos(radian), 0.0},
+		{0.0, 0.0, 0.0, 1.0},
+	})
+}
+
+func RotationZ(radian float64) Matrix4 {
+	return NewMatrix4From([4][4]float64{
+		{math.Cos(radian), -math.Sin(radian), 0.0, 0.0},
+		{math.Sin(radian), math.Cos(radian), 0.0, 0.0},
+		{0.0, 0.0, 1.0, 0.0},
 		{0.0, 0.0, 0.0, 1.0},
 	})
 }
--- core/transformation_test.go
+++ core/transformation_test.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"math"
 	"testing"
 )
 
@@ -50,5 +51,65 @@ func TestReflectiongMatrix(t *testing.T) {
 
 	if !res.EqualTuple(expected) {
 		t.Errorf("scale matrix : expected %v, get %v", expected, res)
+	}
+}
+
+func TestRotationAroundX(t *testing.T) {
+	p := NewPoint(0.0, 1.0, 0.0)
+	half_quarter := RotationX(math.Pi / 4)
+	full_quarter := RotationX(math.Pi / 2)
+
+	res1 := half_quarter.DotTuple(p)
+	res2 := full_quarter.DotTuple(p)
+
+	expected1 := NewPoint(0.0, math.Sqrt(2.0)/2.0, math.Sqrt(2.0)/2.0)
+	expected2 := NewPoint(0.0, 0.0, 1.0)
+
+	if !res1.EqualTuple(expected1) {
+		t.Errorf("expected tupe is %v, result is %v", expected1, res1)
+	}
+
+	if !res2.EqualTuple(expected2) {
+		t.Errorf("expected tupe is %v, result is %v", expected2, res2)
+	}
+}
+
+func TestRotationAroundY(t *testing.T) {
+	p := NewPoint(0.0, 0.0, 1.0)
+	half_quarter := RotationY(math.Pi / 4)
+	full_quarter := RotationY(math.Pi / 2)
+
+	res1 := half_quarter.DotTuple(p)
+	res2 := full_quarter.DotTuple(p)
+
+	expected1 := NewPoint(math.Sqrt(2.0)/2.0, 0.0, math.Sqrt(2.0)/2.0)
+	expected2 := NewPoint(1.0, 0.0, 0.0)
+
+	if !res1.EqualTuple(expected1) {
+		t.Errorf("expected tupe is %v, result is %v", expected1, res1)
+	}
+
+	if !res2.EqualTuple(expected2) {
+		t.Errorf("expected tupe is %v, result is %v", expected2, res2)
+	}
+}
+
+func TestRotationAroundZ(t *testing.T) {
+	p := NewPoint(0.0, 1.0, 0.0)
+	half_quarter := RotationZ(math.Pi / 4)
+	full_quarter := RotationZ(math.Pi / 2)
+
+	res1 := half_quarter.DotTuple(p)
+	res2 := full_quarter.DotTuple(p)
+
+	expected1 := NewPoint(-math.Sqrt(2.0)/2.0, math.Sqrt(2.0)/2.0, 0.0)
+	expected2 := NewPoint(-1.0, 0.0, 0.0)
+
+	if !res1.EqualTuple(expected1) {
+		t.Errorf("expected tupe is %v, result is %v", expected1, res1)
+	}
+
+	if !res2.EqualTuple(expected2) {
+		t.Errorf("expected tupe is %v, result is %v", expected2, res2)
 	}
 }
