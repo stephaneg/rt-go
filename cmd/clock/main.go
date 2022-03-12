3c331a67c247abc92b5d27db33abf2da35735797
Stephane <stephstephg@gmail.com>
2022-03-12 15:16:39 +0100

    complete clock

--- cmd/clock/main.go
+++ cmd/clock/main.go
@@ -8,27 +8,23 @@ import (
 )
 
 func main() {
-	//origin := core.NewPoint(0.0, 0.0, 0.0)
-	r := core.RotationY(3.0 * math.Pi / 6.0)
-
-	twelve := core.NewPoint(0.0, 0.0, 1.0)
-
-	three := r.DotTuple(twelve)
+	r := core.RotationY(2.0 * math.Pi / 12.0)
+	var x, y uint
+	current := core.NewPoint(0.0, 0.0, 1.0)
 
 	c := core.NewCanvas(800, 800)
 	color := core.NewColor(1.0, 0.0, 0.0)
-
 	radius := 3.0 / 8.0 * 800.0
 
-	var x, y uint
-	x = uint(twelve.X() + radius)
-	y = uint(twelve.Z() + radius)
-	c.Write(x, y, color)
+	for i := 0; i < 12; i++ {
+		x = uint(current.X()*radius + 400)
+		y = uint(current.Z()*radius + 400)
+		c.Write(x, y, color)
 
-	x = uint(three.X() + radius)
-	y = uint(three.Z() + radius)
-	c.Write(x, y, color)
+		current = r.DotTuple(current)
+	}
 
 	s := c.ToPPM()
 	fmt.Print(s)
+
 }
